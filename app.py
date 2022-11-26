b4860ca959832efe51e827877896c548e23e6a20
{osaki0329} <{souta0329.y@docomo.ne.jp}>
2022-11-26 14:09:37 +0900

    'データベースと接続'

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
@@ -10,9 +11,25 @@ class Post(db.Model):
   content = db.Column(db.String(100), nullable=False)
   due= db.Column(db.DateTime, nullable=False)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+  if request.method == "GET":
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
+  else:
+    content = request.form.get('content')
+    due = request.form.get('due')
+    
+    due = datetime.strptime(due, '%Y-%m-%d')
+    new_post = Post(content=content, due=due)
+    
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect('/')
+  
+@app.route('/create')
+def create():
+  return render_template('create.html')
 
 if __name__ == "__main__":
   app.run(debug=True)
